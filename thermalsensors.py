cecdc17c06f637ca3bd9f41600fae9ee0693edef
Francesco <francesco.andreetto97@gmail.com>
2024-03-15 11:02:23 +0100

    Update comments: end date in Rome

--- thermalsensors.py
+++ thermalsensors.py
@@ -2,7 +2,7 @@
 
 # This file contains the Class Thermal_Sensors
 # Use this Class with the new version of the pipeline for functional verification of LSPE-STRIP (2024).
-# August 15th 2023, Brescia (Italy) - March 15th 2024, Bologna (Italy)
+# August 15th 2023, Brescia (Italy) - March 15th 2024, Roma (Italy)
 
 # Libraries & Modules
 import logging
